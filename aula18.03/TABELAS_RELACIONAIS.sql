ebec56bd78f77b7d791087aac15f98f71e0583af
JulianaCRodrigues <81633291+JulianaCRodrigues@users.noreply.github.com>
2024-03-18 22:38:11 -0300

    Criar entidade Departamento

---
+++ aula18.03/TABELAS_RELACIONAIS.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE empregados;
+USE empregados;
+CREATE TABLE departamento(
+	cod_depto int not null auto_increment,
+    nome varchar(30) not null,
+    constraint pk_departamento primary key(cod_depto),
+    
+    
+    
+);
